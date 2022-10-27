66bd9b0a3698d27f07babfb69f362a5f7c1cd946
name <email>
2022-10-27 16:34:27 -0700

    does work?

--- src/lib/GameGUI.svelte
+++ src/lib/GameGUI.svelte
@@ -1,56 +1,39 @@
 <script>
-    import Game1 from "./games/Game1.svelte";
-  
-    export let page;
+    import Game1 from './games/Game1.svelte';
 
+    export let page;
 </script>
 
-  <h1 class="mb-12">
+<h1 class="mb-12">
     Excercise {page}
-  </h1>
+</h1>
 
 <div class="flex flex-row w-full mx-0">
     <div class="p-4 border-none border-slate-700">
         <div class="flex flex-col gap-y-4  flex-shrink">
-          <button on:click={_ => (page = 0)}>Home</button>
-          {#each {length:3} as _, i}
-            {#if page!=i+1}
-            <button disabled={i!=0} on:click={_ => (page = (i+1))}>Exercise {i+1}</button>
-          {/if}
-          {/each}
+            <button on:click={_ => (page = 0)}>Home</button>
+            {#each { length: 3 } as _, i}
+                {#if page != i + 1}
+                    <button disabled={i != 0} on:click={_ => (page = i + 1)}>Exercise {i + 1}</button>
+                {/if}
+            {/each}
         </div>
     </div>
 
-  <div class="w-96 h-96 bg-slate-500 grid place-items-center" >
-    <Game1/>
-  </div>
-
-
-  
+    <div class="bg-slate-500 grid place-items-center">
+        <Game1 />
+    </div>
 </div>
 <div>
-  <div class="prose dark:prose-invert text-left border-t pt-8 mt-8 border-slate-600">
-    <h2>
-      How to play
-    </h2>
-    <ul>
-
-      <li>
-              Use the mouse to trace the letter displayed on the screen.
-      </li>
-      <li>
-Stay on the line as best as you can for full points
-        
-      </li>
-      <li>
-Complete in a timely manner for full points
-        
-      </li>
-      
-    </ul>
-  </div>
+    <div class="prose dark:prose-invert text-left border-t pt-8 mt-8 border-slate-600">
+        <h2>How to play</h2>
+        <ul>
+            <li>Use the mouse to trace the letter displayed on the screen.</li>
+            <li>Stay on the line as best as you can for full points</li>
+            <li>Complete in a timely manner for full points</li>
+        </ul>
+    </div>
 </div>
 
 <style>
-  
 </style>
--- src/lib/games/Game1.svelte
+++ src/lib/games/Game1.svelte
@@ -1,36 +1,42 @@
 <script>
     import P5 from 'p5-svelte';
 
-  export const letters = "abcdefghijklmnopqrstuvwxyz".split()
+    const letters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
 
-     const sketch = (p5) => {
-    let x = 0;
-    let y = 0;
-    let size = 15;
-    let threshold = 0;
+    /**
+    //  * @type {(Sketch)=>any}
+     */
+    const sketch = p5 => {
+        let x = 0;
+        let y = 0;
+        let size = 15;
+        let threshold = 0;
 
-    p5.setup = () => {
-      p5.createCanvas(400, 400);
+        p5.setup = () => {
+            p5.createCanvas(400, 400);
+        };
+
+        p5.draw = () => {
+            p5.textSize(80);
+            p5.textAlign(p5.CENTER);
+            p5.text(letters[0], 200, 200);
+            // p5.fill(0, 102, 153);
+            // p5.stroke(1);
+            // threshold = p5.random(0.75);
+
+            // if (threshold < 0.1) p5.line(x, y, x + size, y + size);
+            // else if (0.505 > threshold && threshold > 0.5) p5.line(x, y, x, y + size);
+            // else p5.line(x, y + size, x + size, y);
+
+            // x = x + size;
+            // if (x > p5.width) {
+            //     x = 0;
+            //     y = y + size;
+            // }
+        };
     };
-
-    p5.draw = () => {
-      p5.stroke(1);
-      threshold = p5.random(0.75);
-
-      if (threshold < 0.1) p5.line(x, y, x + size, y + size);
-      else if (0.505 > threshold > 0.5) p5.line(x, y, x, y + size);
-      else p5.line(x, y + size, x + size, y);
-
-      x = x + size;
-      if (x > p5.width) {
-        x = 0;
-        y = y + size;
-      }
-    };
-  };
-
 </script>
 
-  <div class=" w-full h-full border-slate-500 border">
-        <P5 {sketch} />
-  </div>
+<div class=" w-full h-full border-slate-500 border">
+    <P5 {sketch} />
+</div>
